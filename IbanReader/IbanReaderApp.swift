d890344e775713425d6f916b5ed481fc61b98ca0
Aymen Bokri <abokri@igraal.com>
2024-04-25 18:53:16 +0200

    Create project with XCode

---
+++ IbanReader/IbanReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  IbanReaderApp.swift
+//  IbanReader
+//
+//  Created by Aymen Bokri on 25/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct IbanReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
