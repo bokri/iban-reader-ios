ec3ce2333b2c58aafb8ae27ffb713cf30f420722
Aymen Bokri <abokri@igraal.com>
2024-04-25 19:28:01 +0200

    Add No Permission View

---
+++ IbanReader/NoPermission/NoPermissionView.swift
@@ -0,0 +1,43 @@
+//
+//  NoPermissionView.swift
+//  IbanReader
+//
+//  Created by Aymen Bokri on 25/04/2024.
+//
+
+import SwiftUI
+
+/// View displayed when camera permissions are not granted.
+struct NoPermissionView: View {
+    /// View model for handling actions and logic related to the NoPermissionView.
+    @State var viewModel: NoPermissionViewModel
+    
+    var body: some View {
+        VStack {
+            // Large camera icon
+            Image(systemName: "camera.fill")
+                .font(.system(size: 70))
+                .foregroundColor(Color(.primary))
+                .padding(.bottom, 20)
+            
+            // Text indicating camera permissions not granted
+            Text(String(localized: "camera_permissions_not_granted"))
+                .foregroundColor(Color(.primary))
+                .frame(alignment: .center)
+                .font(.title2)
+                .padding(.bottom, 30)
+            
+            // Button to redirect to app settings
+            Button(action: {
+                viewModel.openAppSettings()
+            }) {
+                // Styled button with gear icon and localized title
+                RoundedView(iconName: "gear",
+                            title: String(localized: "go_to_settings"),
+                            isInversed: true,
+                            isSmall: true)
+            }
+        }
+        .padding()
+    }
+}
---
+++ IbanReader/NoPermission/NoPermissionViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  NoPermissionViewModel.swift
+//  IbanReader
+//
+//  Created by Aymen Bokri on 25/04/2024.
+//
+
+import SwiftUI
+
+/// View model for handling actions and logic related to the NoPermissionView.
+@Observable
+class NoPermissionViewModel {
+    /// Opens the app's settings.
+    func openAppSettings() {
+        guard let settingsUrl = URL(string: UIApplication.openSettingsURLString),
+              UIApplication.shared.canOpenURL(settingsUrl) else {
+            return
+        }
+        UIApplication.shared.open(settingsUrl)
+    }
+}
