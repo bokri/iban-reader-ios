333ca11d36a03a98334377c67cb739ac2945be7f
Aymen Bokri <abokri@igraal.com>
2024-04-25 20:36:16 +0200

    Add UI Test to test a basic scenario

--- IbanReaderUITests/IbanReaderUITests.swift
+++ IbanReaderUITests/IbanReaderUITests.swift
@@ -9,33 +9,40 @@ import XCTest
 
 final class IbanReaderUITests: XCTestCase {
 
+    var app: XCUIApplication!
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    func testIBANScanningAndInput() throws {
+        // Step 1: Launch App
+        XCTAssertTrue(app.isDisplayingTabBar)
+
+        // Step 2: Navigate to IBAN Scanning Screen
+        let wireTab = app.tabBars.buttons["Virements"]
+        wireTab.tap()
+        XCTAssertTrue(app.isDisplayingIBANScanningScreen)
+
+        // Step 3: Enter IBAN Manually
+        let ibanTextField = app.textFields["FR76 XXXX"]
+        ibanTextField.tap()
+        ibanTextField.typeText("FR7630006000011234567890189") // Enter a valid IBAN
+        XCTAssertTrue(ibanTextField.value as! String == "FR76 3000 6000 0112 3456 7890 189") // Verify formatting
+        
     }
 }
+
+// Extensions for UI Test Helpers
+extension XCUIApplication {
+    var isDisplayingTabBar: Bool {
+        return tabBars.count > 0
+    }
+
+    var isDisplayingIBANScanningScreen: Bool {
+        return staticTexts["Scanner"].exists
+    }
+}
+
