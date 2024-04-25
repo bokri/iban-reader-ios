12577346aa99da949ec557f7c493fd39c78bdbbd
Aymen Bokri <abokri@igraal.com>
2024-04-25 18:57:09 +0200

    Create TabView with Placeholders

--- IbanReader/IbanReaderApp.swift
+++ IbanReader/IbanReaderApp.swift
@@ -9,9 +9,51 @@ import SwiftUI
 
 @main
 struct IbanReaderApp: App {
+    // MARK: - Initialization
+    
+    init() {
+        // Configure appearance of the navigation bar.
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    }
+    
+    // MARK: - Body
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            // Main navigation view containing the tab view.
+            NavigationView {
+                // Tab view with different sections of the app.
+                TabView {
+                    // Placeholder text for the "Accounts" tab.
+                    Text("accounts")
+                        .tabItem {
+                            Label("accounts", systemImage: "wallet.pass")
+                        }
+                    
+                    // Main content view for scanning IBANs.
+                    ContentView()
+                        .tabItem {
+                            Label("wire", systemImage: "arrow.left.arrow.right")
+                        }
+                    
+                    // Placeholder text for the "Help" tab.
+                    Text("help")
+                        .tabItem {
+                            Label("help", systemImage: "bubble.left.and.bubble.right")
+                        }
+                    
+                    // Placeholder text for the "More" tab.
+                    Text("more")
+                        .tabItem {
+                            Label("more", systemImage: "ellipsis")
+                        }
+                }
+                // Set accent color for tab view.
+                .accentColor(Color(.primary))
+            }
         }
     }
 }
