d4b308b0a32ba67f9cb57f848870877272c873a0
Aymen Bokri <abokri@igraal.com>
2024-04-25 19:42:17 +0200

    Add Content View

--- IbanReader/ContentView.swift
+++ IbanReader/ContentView.swift
@@ -6,16 +6,68 @@
 //
 
 import SwiftUI
+import VisionKit
 
 struct ContentView: View {
+
+    // MARK: - Properties
+
+    /// State variable to store the entered IBAN.
+    @State var iban: String = ""
+
+    // MARK: - Body
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                // Title for IBAN scanning section.
+                Text("scan_import_iban_title")
+                    .foregroundStyle(.text)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                // Navigation link to IBAN scanner view if supported.
+                if DataScannerViewController.isSupported {
+                    NavigationLink {
+                        IBanScannerView(viewModel: IbanScannerViewModel(onResultFinished: onResultFinished(_:)))
+                    } label: {
+                        // Button to trigger IBAN scanning.
+                        RoundedView(iconName: "camera.fill",
+                                    title: String(localized: "scan_cta"),
+                                    isInversed: false,
+                                    isSmall: true)
+                    }.padding()
+                }
+                
+                // Text field for manual IBAN entry.
+                IbanTextField(placeHolder: String(localized: "iban_placeholder"), errorMessage: String(localized: "iban_error"), value: $iban) { input in
+                    input.isFrenchIBAN()
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .background(Color(.background))
+            .navigationTitle("add_beneficiary")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
+        .accentColor(Color(.primary))
+        .onChange(of: iban) { _, newValue in
+            // Automatically format the IBAN as French IBAN if valid.
+            if newValue.isFrenchIBAN() {
+                iban = newValue.formatAsFrenchIBAN() ?? newValue
+            }
+        }
+    }
+    
+    // MARK: - Functions
+    
+    /**
+     Callback function invoked when the IBAN scanning result is finished.
+     
+     - Parameter iban: The scanned IBAN result.
+     */
+    func onResultFinished(_ iban: String) {
+        self.iban = iban
     }
 }
 
